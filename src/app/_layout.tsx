b2013eccfc88c4d448f7429019b572b6f84b7120
kkitov <k.kitov@ibs.bg>
2026-02-27 20:03:35 +0200

    authentication router added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,34 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
+    const router = useRouter();
+    const [isReady, setIsReady] = useState(false);
+    const isAuthenticated = false; // This should ideally come from context or a state management solution.
+
+    useEffect(() => {
+        // Ensure that the router is fully ready before performing navigation
+        if (isReady) {
+            if (isAuthenticated) {
+                router.replace('/(tabs)');
+            } else {
+                router.replace('/(auth)/login');
+            }
+        }
+    }, [isReady]); // Trigger only after the router is ready
+
+    useEffect(() => {
+        // Mark the router as ready after the component mounts
+        setIsReady(true);
+    }, []);
+
     return (
-        <Stack screenOptions={{
-            headerShown: false
-        }}
-        >
-            <Stack.Screen name="(tabs)" />
-        </Stack>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(tabs)" />
+                <Stack.Screen name="(auth)" />
+            </Stack>
+        </SafeAreaView>
     );
 }
