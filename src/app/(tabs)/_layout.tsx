f6a5e3e656b63f9c2a2955edeec9cd1a87d721c2
kkitov <k.kitov@ibs.bg>
2026-02-28 19:56:37 +0200

    reworked tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,21 +1,22 @@
 import { Tabs } from 'expo-router';
-import {Ionicons} from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import {Colors} from '../../constants/colors'; 
 
 export default function TabsLayout() {
     return (
-        <Tabs screenOptions={{ tabBarActiveTintColor: "coral"}}>
-            <Tabs.Screen name="index" 
-            options={{
-                title: "first page",
-                tabBarIcon: ({color, size, focused}) => <Ionicons name={focused ? "home" : "home-outline"} size={size} color={color} />
-            }} />
-            <Tabs.Screen name="secondPage" options={{ 
-                title: "second page",
-                tabBarIcon: ({color, size, focused}) => <Ionicons name={focused ? "information-circle" : "information-circle-outline"} size={size} color={color} />
-            }} />
-            <Tabs.Screen name="thirdPage" options={{ 
-                title: "third page", 
-                tabBarIcon: ({color, size, focused}) => <Ionicons name={focused ? "person" : "person-outline"} size={size} color={color} /> }} />
+        <Tabs screenOptions={{ tabBarActiveTintColor: Colors.button }}>
+            <Tabs.Screen name="index"
+                options={{
+                    title: "Jobs",
+                    tabBarIcon: ({ color, size, focused }) => <MaterialIcons name={focused ? "work" : "work-outline"} size={size} color={color} />
+                }}
+            />
+            <Tabs.Screen name="thirdPage" options={{
+                title: "Professionals",
+                tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? "person" : "person-outline"} size={size} color={color} />
+            }}
+            />
         </Tabs>
     )
 }
